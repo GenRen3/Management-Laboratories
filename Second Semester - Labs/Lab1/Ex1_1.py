de0997bc6b70f170a811450039d95be796298d4e
Gennaro <gennaro.rende@yahoo.com>
2019-05-08 11:53:36 +0200

    adding files

---
+++ Second Semester - Labs/Lab1/Ex1_1.py
@@ -0,0 +1,93 @@
+
+#first use undirected graph
+#then import directed graph
+#strongly connected component
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+import random
+
+random.seed(17)
+G = nx.Graph()
+
+#create and print small subset of the graph
+#cnt=0
+#with open("names.txt", "r") as file:
+    #for row in file:
+        #if cnt<100:
+            #node = row.split()
+            #G.add_edge(node[1], node[2])
+            #cnt+=1
+#draw subgraph
+#nx.draw(G, with_labels=True)
+#plt.show()
+
+#create graph
+with open("./names.txt", "r") as file:
+    for row in file:
+        node = row.split()
+        G.add_edge(node[1], node[2])
+
+
+print("number of nodes = ", nx.number_of_nodes(G))
+print("number of edges = ", nx.number_of_edges(G))
+
+
+all_nodes = list(nx.nodes(G))
+infected=set()
+new_infected = set()
+infected.update(set(random.choices(all_nodes,k=100)))
+safe=[]
+
+
+#with probability p=0.7, independently from number of infected neighbors
+for t in range(0,50):
+    #print("len infected run %d at t= %d " %(len(infected), t))
+    for n in infected:
+        found = set(G.neighbors(n))
+        p=np.ones(len(found))
+        p=p*0.7
+        new_infected.update(set(random.choices(list(found), list(p), k=random.randint(0, len(found)))))
+        #print(len(new_infected))
+    infected.update(new_infected)
+    safe.append(len(all_nodes)-len(infected))
+    if t>2 and safe[t] == safe[t-2]:
+        break
+    #print("len infected updated run %d at t= %d " %(len(infected), t))
+
+print(safe)
+y = np.array(safe)
+print(y)
+x= np.arange(0,t+1,1)
+print(x)
+plt.semilogy(x,y)
+plt.title("Survival function")
+plt.xlabel("time")
+plt.ylabel("Survivors")
+plt.show()
+
+
+
+infected=set()
+new_infected = set()
+infected.update(set(random.choices(all_nodes,k=100)))
+
+for n in infected:
+    found = set(G.neighbors(n))
+    for f in found:
+        f_neigh = set(G.neighbors(f))
+        f_inf =
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Second Semester - Labs/Lab1/Ex1_2.py
@@ -0,0 +1,83 @@
+
+
+#first use undirected graph
+#then import directed graph
+#strongly connected component
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import statistics
+
+G = nx.Graph()
+
+#create and print small subset of the graph
+#cnt=0
+#with open("names.txt", "r") as file:
+    #for row in file:
+        #if cnt<100:
+            #node = row.split()
+            #G.add_edge(node[1], node[2])
+            #cnt+=1
+#draw subgraph
+#nx.draw(G, with_labels=True)
+#plt.show()
+
+#create graph
+with open("names.txt", "r") as file:
+    for row in file:
+        node = row.split()
+        G.add_edge(node[1], node[2])
+
+
+print("number of nodes = ", nx.number_of_nodes(G))
+print("number of edges = ", nx.number_of_edges(G))
+
+
+#plot degree distribution
+#for each node returns the degree
+degrees = G.degree()
+# max_deg=0
+# for i in list(G.nodes):
+#     if G.degree(i) > max_deg:
+#         max_deg = G.degree(i)
+#         max_profile = i
+# print("profile with highest degree: ", max_profile)
+
+sort_node = sorted([d for n,d in degrees], reverse=True) #degrees sorted from largest to smallest
+plt.loglog(sort_node, marker = 'o') #dovrebbe essere al contrario, con gli assi invertiti...
+plt.title("Degree distribution")
+plt.xlabel("Nodes")
+plt.ylabel("Degree")
+plt.show()
+
+#compute average degree distribution
+deg_list = [degrees(i) for i in list(G.nodes)]
+average = np.mean(deg_list)
+print("Average degree distribution = ", average)
+
+#print clustering coefficient
+print("Clustering coefficient = ", nx.average_clustering(G))
+
+#print size of giant component
+giant = max(nx.connected_components(G))
+print("Size of giant component = ", len(giant))
+
+
+
+#Directed Graph
+DiG = nx.DiGraph()
+with open("names.txt", "r") as file:
+    for row in file:
+        node = row.split()
+        DiG.add_edge(node[1], node[2])
+
+print("number of nodes of Directed Graph = ", nx.number_of_nodes(DiG))
+print("number of edges of Directed Graph = ", nx.number_of_edges(DiG))
+
+strongly_conn = max(nx.strongly_connected_component_subgraphs(DiG), key=len)
+print("Size of largest strongly connected component = ", len(strongly_conn))
+# print("Radius of strongly connected component = ", nx.radius(strongly_conn)) #non funziona, perché???
+
+weakly_conn = max(nx.weakly_connected_component_subgraphs(DiG), key=len)
+print("Size of largest weakly connected component = ", len(weakly_conn))
