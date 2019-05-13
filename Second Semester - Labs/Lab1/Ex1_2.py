1f12efcb69afe07edc099ec8fe1ec74eff82ffcd
Gennaro <gennaro.rende@yahoo.com>
2019-05-13 12:04:27 +0200

    Final changes to the code

--- Second Semester - Labs/Lab1/Ex1_2.py
+++ Second Semester - Labs/Lab1/Ex1_2.py
@@ -9,21 +9,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import statistics
 import random
+import pylab
 
 random.seed(17)
 G = nx.Graph()
-
-#create and print small subset of the graph
-#cnt=0
-#with open("names.txt", "r") as file:
-    #for row in file:
-        #if cnt<100:
-            #node = row.split()
-            #G.add_edge(node[1], node[2])
-            #cnt+=1
-#draw subgraph
-#nx.draw(G, with_labels=True)
-#plt.show()
 
 #create graph
 with open("./names.txt", "r") as file:
@@ -35,68 +24,79 @@ with open("./names.txt", "r") as file:
 print("number of nodes = ", nx.number_of_nodes(G))
 print("number of edges = ", nx.number_of_edges(G))
 
+n_iteration = 0
 
-all_nodes = list(nx.nodes(G))
-infected=set()
-new_infected = set()
-infected.update(set(random.choices(all_nodes,k=100)))
-safe=[]
+while n_iteration != 3:
+    all_nodes = list(nx.nodes(G))
+    infected=set()
+    new_infected = set()
+    inf = input("Enter number of infected: ")
+    inf=int(inf)
+    infected.update(set(random.choices(all_nodes,k=inf)))
+    safe=[]
 
+    prob = input("Enter probability of infection: ")
 
-#with probability p=0.7, independently from number of infected neighbors
-for t in range(0,50):
-    #print("len infected run %d at t= %d " %(len(infected), t))
-    for n in infected:
-        found = set(G.neighbors(n))
-        p=np.ones(len(found))
-        p=p*0.7
-        new_infected.update(set(random.choices(list(found), list(p), k=random.randint(0, len(found)))))
-        #print(len(new_infected))
-    infected.update(new_infected)
-    safe.append(len(all_nodes)-len(infected))
-    if t>2 and safe[t] == safe[t-2]:
-        break
-    #print("len infected updated run %d at t= %d " %(len(infected), t))
+    #with probability p=0.7, independently from number of infected neighbors
+    for t in range(0,50):
+        #print("len infected run %d at t= %d " %(len(infected), t))
+        for n in infected:
+            found = set(G.neighbors(n))
+            p=np.ones(len(found))
+            prob=float(prob)
+            p=p*prob #WE NEED TO VARY THIS VARIABLE
+            new_infected.update(set(random.choices(list(found), list(p), k=random.randint(0, len(found)))))
+            #print(len(new_infected))
+        infected.update(new_infected)
+        safe.append(len(all_nodes)-len(infected))
+        if t>2 and safe[t] == safe[t-2]:
+            break
+        #print("len infected updated run %d at t= %d " %(len(infected), t))
 
-print(safe)
-y = np.array(safe)
-print(y)
-x= np.arange(0,t+1,1)
-print(x)
-plt.semilogy(x,y)
+    print(safe)
+    y = np.array(safe)
+    x = np.arange(0,t+1,1)
+    plt.semilogy(x,y, label="Probability:"+str(prob)+" New Infections:"+str(inf)+"")
+    n_iteration+=1
+
 plt.title("Survival function")
-plt.xlabel("time")
+plt.xlabel("Time")
 plt.ylabel("Survivors")
+plt.legend()
 plt.show()
 
 
-#considering percentage of infected neighbors
-infected=set()
-new_infected = set()
-infected.update(set(random.choices(all_nodes,k=100)))
-safe=[]
+#HERE WE USED A DIFFERENT APPROACH BUT THE PREVIOUS ONE, IS THE ONE WE CHOSE TO APPLY
+# #considering percentage of infected neighbors
+# infected=set()
+# new_infected = set()
+# infected.update(set(random.choices(all_nodes,k=100)))
+# safe=[]
 
-for t in range(0,30):
-    for n in infected:
-        found = set(G.neighbors(n))
-        for f in found:
-            f_neigh = set(G.neighbors(f))
-            number_inf = len(f_neigh & infected)
-            inf_flag = random.randrange(number_inf)
-            if inf_flag>0:
-                new_infected.add(f)
-    infected.update(new_infected)
-    safe.append(len(all_nodes) - len(infected))
-    if t > 2 and safe[t] == safe[t - 2]:
-        break
+# for t in range(0,30):
+#     for n in infected:
+#         found = set(G.neighbors(n))
+#         for f in found:
+#             f_neigh = set(G.neighbors(f))
+#             number_inf = len(f_neigh & infected)
+#             inf_flag = random.randrange(number_inf)
+#             if inf_flag>0:
+#                 new_infected.add(f)
+#     infected.update(new_infected)
+#     safe.append(len(all_nodes) - len(infected))
+#     if t > 2 and safe[t] == safe[t - 2]:
+#         break
 
-print(safe)
-y = np.array(safe)
-print(y)
-x= np.arange(0,t+1,1)
-print(x)
-plt.semilogy(x,y)
-plt.title("Survival function")
-plt.xlabel("time")
-plt.ylabel("Survivors")
-plt.show()
+# print(safe)
+# y = np.array(safe)
+# print(y)
+# x= np.arange(0,t+1,1)
+# print(x)
+# plt.semilogy(x,y)
+# plt.title("Survival function")
+# plt.xlabel("time")
+# plt.ylabel("Survivors")
+# plt.show()
+
+
+#plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
