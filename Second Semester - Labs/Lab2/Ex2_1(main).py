03c55d504fd1f8815590a2747c8ad01c54465183
Gennaro <gennaro.rende@yahoo.com>
2019-05-15 09:29:57 +0200

    structure

--- Second Semester - Labs/Lab2/Ex2_1(main).py
+++ Second Semester - Labs/Lab2/Ex2_1(main).py
@@ -1,1 +1,8 @@
 import simpy
+import networkx as nx
+import numpy as np
+import matplotlib.pylab as plt
+
+
+if __name__ == '__main__':
+    main()
---
+++ Second Semester - Labs/Lab2/Ex2_1(server).py
@@ -0,0 +1,4 @@
+import simpy
+import networkx as nx
+import numpy as np
+import matplotlib.pylab as plt
