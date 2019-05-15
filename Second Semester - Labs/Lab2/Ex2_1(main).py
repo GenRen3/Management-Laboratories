9b9db4691090b729cfd12a088185dfc55ce00f8e
Gennaro <gennaro.rende@yahoo.com>
2019-05-15 10:06:35 +0200

    hey

--- Second Semester - Labs/Lab2/Ex2_1(main).py
+++ Second Semester - Labs/Lab2/Ex2_1(main).py
@@ -3,6 +3,13 @@ import networkx as nx
 import numpy as np
 import matplotlib.pylab as plt
 
+class Json_creator(object):
+
+    def main():
+        N_c=
+        N_s =
+
+
 
 if __name__ == '__main__':
     main()
