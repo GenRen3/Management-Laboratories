ea616be8c032c75eacc0ccd67dcd4624e1a3e849
Marzia <s260321@studenti.polito.it>
2019-05-27 12:47:45 +0200

    up

--- Second Semester - Labs/Lab2/Lab2_management.py
+++ Second Semester - Labs/Lab2/Lab2_management.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 
 import simpy
 import random
@@ -14,25 +14,16 @@ import time
 # Constants
 #------------
 RANDOM_SEED = 17
-SIM_TIME = 10
+SIM_TIME = 100
 LINK_CAPACITY = 10 #Gb
 MAX_REQ = 10
-SERVER_NUM = 3
-lambda_NA = 60#the higher it is, the higher the num of clients
-lambda_SA = 40
-lambda_EU = 60
-lambda_AF = 20
-lambda_AS = 50
-lambda_OC = 30
-mu_us = 1/25
-mu_eu = 1/30
-mu_as = 1/40
-#mu_NA =
-#mu_SA =
-#mu_EU =
-#mu_AF =
-#mu_AS =
-#mu_OC =
+#SERVER_NUM = 3
+lambda_NA = 2#the higher it is, the higher the num of clients
+lambda_SA = 1
+lambda_EU = 2
+lambda_AF = 1
+lambda_AS = 1.5
+lambda_OC = 1.5
 
 #ORIGIN = 'SA' #(we can use NA,SA,EU,AF,AS,OC)
 
@@ -74,86 +65,67 @@ class Client(object):
         self.position = position
         self.env.process(self.run())
 
-
-
     def run(self):
         time_arrival = self.env.now
-        print("Client from ", self.position, "arrived at ", time_arrival)
+        random.seed(time.clock())
+        K = random.randint(1,3)
+        print("Client ", self.number, "from ", self.position, "arrived at ", time_arrival, "with ", K, "requests")
         # map.get_data_clients()
         # map.get_data_servers()
         #tot_list_servers = map.get_list_servers()
         [lat_client,long_client] = map.get_random_client(self.position) #with this line we get a random client
         nearest_servers = map.get_nearest_servers(lat_client,long_client) #with this line we get the nearset servers to the chosen client
-        print(nearest_servers)
+        #print(nearest_servers)
         #map.get_map_links(nearest_servers,lat_client,long_client)
-        random.seed(time.clock())
-        K = random.randint(10,100)
         count_req = 1
         while count_req <= K:
             ok = 0
             while ok == 0:
                 yield self.env.process(self.env.servers.serve(nearest_servers))
+                ok = self.env.servers.success_req()
             # size = 1000 #size = random.randint(1000,1400)
-            # routing_table = 'US1'
-            # ok=0
-            # while ok==0:
-            #     for server_code in routing_table:
-            #         yield self.env.process(self.env.servers.serve(server_code,size))
-            #         if ok==1:
-            #             break #set timeout??
             count_req+=1
             #calculate response time
-            self.env.stats.push(self.env.now-time_arrival)
+            #self.env.stats.push(self.env.now-time_arrival)
         self.tot_time = self.env.now-time_arrival
-#nella funzione mappa fai classe che genera posizione nella zona designata e calcola distanza e routing table
-
+        print("Client ", self.number, "from ", self.position, "served in ", self.tot_time)
+#ok non funziona
 
 class Server(object):
     # we drop requests if everything is full
-   def __init__(self, environment, service_rate, all_servers):
+   def __init__(self, environment, all_servers):
        self.env = environment
-       self.service_rate = service_rate
        self.server_resources = all_servers
-       # self.tot_list_servers = map.get_list_servers()
-       # self.server_resources = {}
-       # for server in self.tot_list_servers: #create dictionary of all servers
-       #     self.server_resources[server]=simpy.Resource(self.env, capacity=MAX_REQ)
-       # print(self.server_resources)
-
+       self.flag = 0
 
 
    def serve(self,list_nearest):
-       ok = 0
        for server in list_nearest: #select the nearest servers
            if self.server_resources[server[0]].count < MAX_REQ:
+               #print(server[0], self.server_resources[server[0]].count)
                with self.server_resources[server[0]].request() as request:
                    yield request
-                   service_time = random.expovariate(lambd=self.service_rate)
+                   server_latency = random.uniform(1, 10)
+                   RTT = float(server[1])/(3*10^8)
+                   # print(RTT)
+                   transfer_delay = random.randint(1, 5)
+                   service_time = server_latency + transfer_delay + RTT
+                   # print(service_time)
                    yield self.env.timeout(service_time) #the timeout must be server_latency+RTT(depending on distance)+transfer_delay (depending on available capacity and packet size)
-               ok = 1 #if request is solved by the server, set ok flag to 1, break from cycle and go back to client for next rquest
+               self.flag = 1 #if request is solved by the server, set ok flag to 1, break from cycle and go back to client for next rquest
+               #print(ok)
                break
-       return ok
 
+   def success_req(self):
+       if self.flag == 0:
+           return 0
+       else:
+           return 1
 
-       # if server_code.count==MAX_REQ:
-       #     ok=0
-       #     return ok
-       # else:
-       #     with self.server_code.request() as request:#bisogna associare il codice a una risorsa(server)
-       #          yield request
-       #          service_time = random.expovariate(lambd=self.service_rate)
-       #          yield self.env.timeout(service_time)
-       #          ok =1
-       #     return ok
-       # if ok==0: return 0 #rejected
-       # else:
-       #     print('ok')
-       #     return 1 #accepted
 
 
 if __name__=='__main__':
     random.seed(RANDOM_SEED)
-    mu = 1.0/20.0
 
     map.get_data_clients()
     map.get_data_servers()
@@ -173,7 +145,7 @@ if __name__=='__main__':
         all_servers[server]=simpy.Resource(env, capacity=MAX_REQ)
     # print(all_servers)
 
-    env.servers = Server(env, mu, all_servers)
+    env.servers = Server(env, all_servers)
     # #start the arrival process
     env.process(arrival(env,'NA'))
     env.process(arrival(env,'SA'))
